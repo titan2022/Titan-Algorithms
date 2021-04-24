f0e0ecd9c024f7b86f9e02a88316828ebfb458f2
cplepel-py <cbplepel@gmail.com>
2021-04-24 15:36:54 -0500

    Made dev runnable

--- dev/src/main/com/titanrobotics2022/Main.java
+++
@@ -1,7 +0,0 @@
-package com.titanrobitcs2022;
-
-public class Main {
-    public static void main(String[] args) {
-        return;
-    }
-}
---
+++ dev/src/main/java/com/titanrobotics2022/Main.java
@@ -0,0 +1,7 @@
+package com.titanrobotics2022;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
