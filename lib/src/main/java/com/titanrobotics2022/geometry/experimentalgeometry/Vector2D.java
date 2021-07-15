a00166483465152917fb2216b59eed3ac7f2b8c7
Teodor Tchalakov <teodor.tch@gmail.com>
2021-07-14 20:46:36 -0500

    Added incomplete generic 2D and 3D vector class

---
+++ lib/src/main/java/com/titanrobotics2022/geometry/experimentalgeometry/Vector2D.java
@@ -0,0 +1,9 @@
+package com.titanrobotics2022.geometry.experimentalgeometry;
+
+public class Vector2D<S extends Vector2DOperation<S>> extends Vector<S>{
+
+    public Vector2D(S headCoordinate)
+    {
+        super(headCoordinate);
+    }
+}
---
+++ lib/src/main/java/com/titanrobotics2022/geometry/experimentalgeometry/Vector3D.java
@@ -0,0 +1,9 @@
+package com.titanrobotics2022.geometry.experimentalgeometry;
+
+public class Vector3D<S extends Vector2DOperation<S>> extends Vector<S>{
+
+    public Vector3D(S headCoordinate)
+    {
+        super(headCoordinate);
+    }    
+}
