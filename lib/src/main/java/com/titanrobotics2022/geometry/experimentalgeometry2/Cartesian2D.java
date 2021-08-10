e1aebafe9b067d76582d97b6fe7d542c41c28be1
Teodor Tchalakov <teodor.tch@gmail.com>
2021-08-09 22:05:24 -0500

    Added cartesian 2D space

---
+++ lib/src/main/java/com/titanrobotics2022/geometry/experimentalgeometry2/Cartesian2D.java
@@ -0,0 +1,10 @@
+package com.titanrobotics2022.geometry.experimentalgeometry2;
+
+// TODO: Turn into singleton
+public class Cartesian2D implements Space {
+
+    @Override
+    public int getDimension() {
+        return 2;
+    }
+}
