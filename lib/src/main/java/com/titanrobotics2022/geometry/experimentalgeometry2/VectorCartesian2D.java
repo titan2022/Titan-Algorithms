3879c664c57ae87f816e2aea61a863499670e253
Teodor Tchalakov <teodor.tch@gmail.com>
2021-08-09 22:05:38 -0500

    Started cartesian 2d vector

---
+++ lib/src/main/java/com/titanrobotics2022/geometry/experimentalgeometry2/VectorCartesian2D.java
@@ -0,0 +1,71 @@
+package com.titanrobotics2022.geometry.experimentalgeometry2;
+
+public class VectorCartesian2D implements Vector<Cartesian2D> {
+
+    @Override
+    public boolean isNaN() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Vector<Cartesian2D> getZero() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector<Cartesian2D> plus(Vector<Cartesian2D> rhs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector<Cartesian2D> minus(Vector<Cartesian2D> rhs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector<Cartesian2D> scalarMultiply(double a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector<Cartesian2D> negate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double magnitude() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double magnitudeSquared() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Vector<Cartesian2D> unitVector() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double dot(Vector<Cartesian2D> rhs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isInfinite() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
