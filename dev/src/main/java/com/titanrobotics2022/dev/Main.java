d9c4e9913cf06a7c08034a0f33623726858d0722
kevinh05 <khyy4321@gmail.com>
2021-10-20 17:50:03 -0500

    message test

--- dev/src/main/java/com/titanrobotics2022/dev/Main.java
+++ dev/src/main/java/com/titanrobotics2022/dev/Main.java
@@ -2,6 +2,6 @@ package com.titanrobotics2022.dev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World new push test");
     }
 }
