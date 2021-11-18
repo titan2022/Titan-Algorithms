018349dbde103f72f417383a91b7ca98aa11f028
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2021-11-17 18:52:31 -0600

    Try out different object detection algorithms

---
+++ lib/src/main/python/titan_algorithms/vision_processing/realsense_demo.py
@@ -0,0 +1,118 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
+
+###############################################
+##      Open CV and Numpy integration        ##
+###############################################
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+def showContours(image, contoursWinName="Contours", threshWinName="Thresh"):
+    color_image = image.copy()
+    _, thresh = cv2.threshold(cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY), 26, 255, cv2.THRESH_BINARY)
+    # run a morphological closing
+    #thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    cnt, heir = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for rect in cnt:
+        x, y, w, h = cv2.boundingRect(rect)
+        if w * h > 100:
+            cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.imshow(contoursWinName, color_image)
+    cv2.imshow(threshWinName, thresh)
+
+def showEdges(img):
+    # Convert to graycsale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Blur the image for better edge detection
+    img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+    
+    '''# Sobel Edge Detection
+    sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
+    sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
+    sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+    # Display Sobel Edge Detection Images
+    cv2.imshow('Sobel X', sobelx)
+    cv2.imshow('Sobel Y', sobely)
+    cv2.imshow('Sobel X Y using Sobel() function', sobelxy)'''
+    
+    # Canny Edge Detection
+    edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
+    # Display Canny Edge Detection Image
+    cv2.imshow('Canny Edge Detection', edges)
+    return edges
+
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        showContours(color_image)
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        showContours(cv2.cvtColor(showEdges(color_image), cv2.COLOR_GRAY2BGR), "Contours of Edges", "_")
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        if cv2.waitKey(1) == ord('q'):  # Press the q button to stop the program
+            cv2.destroyAllWindows()
+            break
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
