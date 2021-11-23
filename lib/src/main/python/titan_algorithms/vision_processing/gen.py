724898e8beb282d6c9c49a58b1fe6ce5c2d82747
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2021-11-22 20:32:06 -0600

    Recognize markers

--- lib/src/main/python/titan_algorithms/vision_processing/gen.py
+++ lib/src/main/python/titan_algorithms/vision_processing/gen.py
@@ -2,7 +2,37 @@
 
 
 import cv2
+import os
+import numpy
+from math import *
+cv = cv2
+np = numpy
 
+
+class Line:
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.x1 = args[0][0]
+            self.y1 = args[0][1]
+            self.x2 = args[1][0]
+            self.y2 = args[1][1]
+        elif len(args) == 4:
+            self.x1, self.y1, self.x2, self.y2 = args
+        else:
+            raise TypeError("Line takes either 2 or 4 arguments, representing the endpoints of the line")
+
+    def __len__(self):
+        return int(self.length)
+
+    @property
+    def length(self):
+        return sqrt(pow(abs(self.x1 - self.x2), 2) + pow(abs(self.y1 - self.y2), 2))
+
+
+try:
+    os.chdir(os.path.dirname(__file__))  # Set current working directory to script folder
+except NameError:
+    print("Running in " + os.getcwd())
 arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 
 # https://www.pyimagesearch.com/2020/12/14/generating-aruco-markers-with-opencv-and-python/
@@ -15,17 +45,43 @@ def draw(id):
 def track(image):
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
-    print(corners, ids, rejected)
+    if len(corners) > 0:
+        for shape, id in zip(corners[0], ids):  # TODO: rename shape to boundingSqr or something
+            shape = shape.astype(numpy.int32)
+            print(shape, id)
+            # IMPORTANT: this assumes that the marker is oriented upright
+            width = Line(shape[0], shape[1]).length
+            height = Line(shape[1], shape[2]).length
+            print(width, 'x', height)
+            # TODO: this doesn't work at all, can't determine which direction or angle
+            print("predicted angle:", atan((height - width) / height) * 180 / pi)
+            cv2.drawContours(image, [shape], 0, (0, 0, 255), 2)
+            cv.line(image, shape[0], shape[1], (255,0,0), 5)
 
 def mainloop():
     while True:
         # Hit 'q' on the keyboard to quit!
         key = cv2.waitKeyEx(30) & 0xFF
-        cv2.imshow('webcam', cap.read()[0])
+        frame = cap.read()[1]
+        track(frame)
+
+        ''' For testing purposes
+        imgray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(imgray, 127, 255, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame, contours, -1, (0, 255, 0))'''
+
+        cv2.imshow('webcam', frame)
         if key == ord('q'):
             break
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    track(draw(0))
-    mainloop()
+    cap = cv2.VideoCapture(0)
+    marker = draw(0)
+    # cv2.imwrite("0.png", marker)
+    track(marker)
+    try:
+        mainloop()
+    finally:
+        cv2.destroyAllWindows()
+        cap.release()
