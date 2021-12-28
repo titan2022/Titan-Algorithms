ea33504d7b133cd8421fdc97ad25e898798d72b3
ttchalakov <teodor.tch@gmail.com>
2021-12-28 13:45:18 -0600

    Documented AbstractPoint

--- lib/src/main/java/com/titanrobotics2022/geometry/AbstractPoint.java
+++ lib/src/main/java/com/titanrobotics2022/geometry/AbstractPoint.java
@@ -1,29 +1,34 @@
 package com.titanrobotics2022.geometry;
 
+/**
+ * Represents a generic geometric point.
+ * @param <S> Type of the coordinate system that the point is defined by.
+ */
 public interface AbstractPoint<S extends CoordinateSystem> {
 
     /**
-     * Returns true if any of the coordinates are NaN.
-     * @return true if any of the coordinates are NaN
+     *  Checks if any of the coordinates are NaN; false otherwise.
+     *  @return True if any of the coordinates are NaN; false otherwise.
      */
     boolean isNaN();
 
     /**
-     * Returns true if any of the coordiantes are positive or negative infinity
-     * @return true if any of the coordiantes are positive or negative infinity
+     *  Checks if any of the coordiantes are positive or negative infinity; false otherwise.
+     *  @return True if any of the coordiantes are positive or negative infinity; false otherwise.
      */
     boolean isInfinite();
 
-    /** Get the space to which the point belongs.
-     * @return containing space
+    /**
+     *  Get the space to which the point belongs.
+     *  @return Returns the defining space type.
      */
     CoordinateSystem getSpace();
 
     /**
-     * Assert that two coordinates are equal with tolerance.
-     * @param rhs Coordinate to be compared to
-     * @param tolerance maximum difference between coordinates
-     * @return true if coordinates are within tolerance of each other
+     *  Asserts that two coordinates are equal with tolerance.
+     *  @param rhs Coordinate to be compared to implicit coordinate.
+     *  @param tolerance Maximum tolerance difference between coordinates.
+     *  @return True if coordinates are within tolerance of each other; false otherwise.
      */
     boolean equals(S rhs, double tolerance);
 }
