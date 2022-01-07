e8be10415be59c4a3c1917572901df127bbf6807
cplepel-py <cbplepel@gmail.com>
2022-01-07 16:09:06 -0600

    Removed unnecessary allocations in Kalman Filter

--- lib/src/main/java/com/titanrobotics2022/sensorfusion/KalmanFilter.java
+++ lib/src/main/java/com/titanrobotics2022/sensorfusion/KalmanFilter.java
@@ -1,11 +1,19 @@
 package com.titanrobotics2022.sensorfusion;
 
+import org.ejml.data.DMatrixRMaj;
 import org.ejml.simple.SimpleMatrix;
+import static org.ejml.dense.row.CommonOps_DDRM.*;
 
 public class KalmanFilter {
-    protected final SimpleMatrix[] means;
-    protected final SimpleMatrix[] covs;
+    protected final DMatrixRMaj[] means;
+    protected final DMatrixRMaj[] covs;
     protected final double[] lastUpdated;
+    private final DMatrixRMaj pred;
+    private final DMatrixRMaj predCov;
+    private final DMatrixRMaj prec;
+    private final DMatrixRMaj z;
+    private double predTime = 0;
+    private int predDegree = -1;
 
     public KalmanFilter(int degree, SimpleMatrix mean, SimpleMatrix cov, double timeOffset) {
         if(degree < 0)
@@ -16,14 +24,18 @@ public class KalmanFilter {
             throw new IllegalArgumentException("Covariance matrix should be square with the same dimension as the mean vector.");
         if(cov.determinant() < 0 || !cov.isIdentical(cov.transpose(), 0))
             throw new IllegalArgumentException("Covariance matrix must be symmetric positive semi-definite.");
-        means = new SimpleMatrix[degree+1];
-        covs = new SimpleMatrix[degree+1];
+        means = new DMatrixRMaj[degree+1];
+        covs = new DMatrixRMaj[degree+1];
         lastUpdated = new double[degree+1];
         for(int i=0; i<degree+1; i++){
-            means[i] = new SimpleMatrix(mean.numRows(), mean.numCols());
-            covs[i] = new SimpleMatrix(cov);
+            means[i] = new DMatrixRMaj(mean.numRows(), mean.numCols());
+            covs[i] = new DMatrixRMaj(cov.getDDRM());
             lastUpdated[i] = timeOffset;
         }
+        pred = means[0].createLike();
+        predCov = covs[0].createLike();
+        prec = covs[0].createLike();
+        z = means[0].createLike();
     }
     public KalmanFilter(int degree, SimpleMatrix mean, SimpleMatrix cov) {
         this(degree, mean, cov, 0.0);
@@ -35,52 +47,40 @@ public class KalmanFilter {
         this(degree, 0.0);
     }
 
-    public void set(int degree, SimpleMatrix mean, SimpleMatrix cov, double time) {
+    public void set(int degree, DMatrixRMaj mean, DMatrixRMaj cov, double time) {
         means[degree].set(mean);
         covs[degree].set(cov);
         lastUpdated[degree] = time;
     }
+    public void set(int degree, SimpleMatrix mean, SimpleMatrix cov, double time) {
+        set(degree, mean.getDDRM(), cov.getDDRM(), time);
+    }
 
-    public SimpleMatrix[] getFullPred(int degree, double time) {
-        SimpleMatrix mean = means[degree].copy();
-        SimpleMatrix cov = covs[degree].copy();
+    protected void calcPred(int degree, double time) {
+        if(predTime == time && predDegree == degree) return;
+        add(means[degree], 0, pred);
+        add(covs[degree], 0, predCov);
         double coef;
         double fact = 1;
         for(int i=degree+1; i<means.length; i++){
             fact *= i;
             coef = Math.pow(time - lastUpdated[i], i-degree) / fact;
-            mean = mean.plus(means[i].scale(coef));
-            cov = cov.plus(covs[i].scale(coef));
+            addEquals(pred, coef, means[i]);
+            addEquals(predCov, coef, covs[i]);
         }
-        return new SimpleMatrix[]{mean, cov};
-    }
-    public SimpleMatrix[] getFullPred(double time) {
-        return getFullPred(0, time);
+        predTime = time;
+        predDegree = degree;
     }
     public SimpleMatrix getPred(int degree, double time) {
-        SimpleMatrix pred = means[degree].copy();
-        double coef;
-        double fact = 1;
-        for(int i=degree+1; i<means.length; i++){
-            fact *= i;
-            coef = Math.pow(time - lastUpdated[i], i-degree) / fact;
-            pred = pred.plus(means[i].scale(coef));
-        }
-        return pred;
+        calcPred(degree, time);
+        return new SimpleMatrix(pred);
     }
     public SimpleMatrix getPred(double time) {
         return getPred(0, time);
     }
     public SimpleMatrix getPredCov(int degree, double time) {
-        SimpleMatrix predCov = covs[degree].copy();
-        double coef;
-        double fact = 1;
-        for(int i=degree+1; i<covs.length; i++){
-            fact *= i;
-            coef = Math.pow(time - lastUpdated[i], i-degree) / fact;
-            predCov = predCov.plus(covs[i].scale(coef));
-        }
-        return predCov;
+        calcPred(degree, time);
+        return new SimpleMatrix(predCov);
     }
     public SimpleMatrix getPredCov(double time) {
         return getPredCov(0, time);
@@ -90,15 +90,22 @@ public class KalmanFilter {
         return lastUpdated[degree];
     }
 
-    public SimpleMatrix update(int degree, SimpleMatrix obs, SimpleMatrix prec, double time) {
+    public boolean update(int degree, DMatrixRMaj obs, DMatrixRMaj obsPrec, double time) {
         if(time < lastUpdated[degree])
-            return null;
-        SimpleMatrix[] prior = getFullPred(degree, time);
-        SimpleMatrix postZ = prior[1].solve(prior[0]).plus(prec.mult(obs));
-        SimpleMatrix postCov = (prior[1].invert().plus(prec)).invert();
-        means[degree] = postCov.mult(postZ);
-        covs[degree] = postCov;
-        lastUpdated[degree] = time;
-        return means[degree];
+            return false;
+        calcPred(degree, time);
+        if(!invertSPD(predCov, prec))
+            pinv(predCov, prec);
+        add(prec, obsPrec, predCov);
+        if(!invertSPD(predCov, covs[degree]))
+            pinv(predCov, covs[degree]);
+        mult(prec, pred, z);
+        multAdd(z, obsPrec, obs);
+        mult(covs[degree], z, means[degree]);
+        predDegree = -1;
+        return true;
+    }
+    public boolean update(int degree, SimpleMatrix obs, SimpleMatrix obsPrec, double time) {
+        return update(degree, obs.getDDRM(), obsPrec.getDDRM(), time);
     }
 }
